0f2f496d74613122acf972c57a35e11c3669718b
formulareddy <dasurireddaiah1@gmail.com>
2026-06-26 23:20:49 +0530

    debug: absolute minimal page to isolate 500

--- src/app/tools/[slug]/page.tsx
+++ src/app/tools/[slug]/page.tsx
@@ -1,14 +1,4 @@
-import { Header } from "@/components/layout/header"
-import { Footer } from "@/components/layout/footer"
-import ToolPageClient from "./client"
-
 export default async function ToolPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
-  return (
-    <>
-      <Header />
-      <ToolPageClient slug={slug} />
-      <Footer />
-    </>
-  )
+  return <div>Tool: {slug}</div>
 }
